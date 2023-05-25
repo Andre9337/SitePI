f0061e03503bb649dcbca00cb7d518620a33d66e
Andre Gracioli <n93377304@gmail.com>
2023-05-25 14:38:31 -0300

    ...

--- app.py
+++ app.py
@@ -1,107 +1,85 @@
-"""
-from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__, static_url_path='/static')
-app.debug = True
-
-
-users = [
-    {"username": "user1", "password": "pass1"},
-    {"username": "user2", "password": "pass2"},
-    {"username": "user3", "password": "pass3"}
-]
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        print(username)
-        password = request.form['password']
-        for user in users:
-            if user['username'] == username and user['password'] == password:
-                return redirect('/home')
-        return render_template('login.html', error='Invalid username or password')
-    else:
-        return render_template('login.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
-app = Flask(__name__, static_url_path='/static')
-app.debug=True
+app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def start():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
-        username=request.form['username']
-        password=request.form['password']
-        if 'entrar' in request.form:
-            if len(username) and len(password)!=0:
-                with open('usuario.txt','w') as f:
-                    f.write(username+" "+password)
-                conn=sqlite3.connect('database.db')
-                cur = conn.cursor()
-                cur.execute("SELECT * FROM users WHERE (name = ? OR email = ?) AND pass = ?;", (username, username, password))
-                match = cur.fetchall()
-                conn.close()
-                if len(match) == 1:
-                    return redirect('/home')
-                return render_template('login.html', error='Invalid username or password')
-            else:
-                return render_template('login.html')
-        elif 'register' in request.form:
-            email=request.form['email']
-            with open('inserts.txt','w') as f:
-                f.write(username+" "+password+" "+email)
-            if len(username) and len(password) and len(email)!=0:
-                conn=sqlite3.connect('database.db')
-                cur=conn.cursor()
-                cur.execute('INSERT INTO users (name, email, pass) VALUES (?,?,?)',(username,email,password))
-                conn.commit()
-                with open('insert.txt','w') as f:
-                    rows=cur.fetchall()
-                    for lines in rows:
-                        f.write(lines[0])
-                conn.close
-                return render_template('login.html',error='Registrado com sucesso')
-            else:
-                return render_template('Register.html',error='Algum dos campos está vazio ou invalido!!!')
-    else:
-        return render_template('login.html')
-    
+    if 'username' in session:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if len(username) and len(password) != 0:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM users WHERE (name = ? OR email = ?) AND pass = ?;", (username, username, password))
+            match = cur.fetchone()
+            conn.close()
+
+            if match:
+                session['username'] = username
+                return redirect(url_for('home'))
+
+        return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('Register.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        if len(username) and len(password) and len(email) != 0:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
+            cur.execute('INSERT INTO users (name, email, pass) VALUES (?,?,?)', (username, email, password))
+            conn.commit()
+            conn.close()
+            return render_template('login.html', error='Registered successfully')
+
+        return render_template('register.html', error='Some fields are empty or invalid')
+
+    return render_template('register.html')
 
 @app.route('/home')
 def home():
-    conn=sqlite3.connect('database.db')
-    data=conn.execute('select * from data').fetchall()
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM data')
+    data = cur.fetchall()
     conn.close()
-    return render_template('home.html',data=data)
+    return render_template('home.html', data=data)
 
 @app.route('/add', methods=['POST'])
 def add():
-    if request.method =='POST':
-        dia=request.form['dia']
-        valor=request.form['valor']
-        desc=request.form['desc']
-        conn=sqlite3.connect('database.db')
-        cur=conn.cursor()
-        cur.execute('INSERT INTO data (dia, val, desc) VALUES (?,?,?)',(dia,valor,desc))
-        conn.commit()
-        conn.close()
-        return redirect('/home')
-    else:
-        return render_template('home.html')
-    
+    if request.method == 'POST':
+        dia = request.form['dia']
+        valor = request.form['valor']
+        desc = request.form['desc']
+
+        if dia and valor and desc:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
+            cur.execute('INSERT INTO data (dia, val, desc) VALUES (?,?,?)', (dia, valor, desc))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('home'))
+
+    return render_template('home.html')
+
 @app.route('/edit/<int:id>', methods=['POST', 'GET'])
 def edit(id):
     conn = sqlite3.connect('database.db')
@@ -109,16 +87,20 @@ def edit(id):
     cur.execute('SELECT * FROM data WHERE id = ?', (id,))
     row = cur.fetchone()
     conn.close()
+
     if request.method == 'POST':
         dia = request.form['dia']
         val = request.form['val']
         desc = request.form['desc']
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        cur.execute('UPDATE data SET dia = ?, val = ?, desc = ? WHERE id = ?', (dia, val, desc, id))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('home'))
+
+        if dia and val and desc:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
+            cur.execute('UPDATE data SET dia = ?, val = ?, desc = ? WHERE id = ?', (dia, val, desc, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('home'))
+
     return render_template('edit.html', row=row)
 
 @app.route('/delete/<int:id>', methods=['POST'])
@@ -133,9 +115,9 @@ def delete(id):
 if __name__ == '__main__':
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS users 
-                   (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, pass TEXT)''')
-    cur.execute('''CREATE TABLE IF NOT EXISTS data 
-                   (id INTEGER PRIMARY KEY AUTOINCREMENT, dia TEXT, val TEXT, desc TEXT)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, pass TEXT)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS data
+    (id INTEGER PRIMARY KEY AUTOINCREMENT, id_user INT,dia TEXT, val TEXT, desc TEXT)''')
     conn.close()
     app.run(debug=True)
